4c1d331e803e627a383186ab840d08ac66e88b4b
Thomas Perkins <thomasperkins1123@gmail.com>
2020-09-10 01:25:56 +0000

    Update for version 0.2 080953d1 of jcover

--- src/jcover/java/com/alibaba/excel/util/ConverterUtilsTest.java
+++ src/jcover/java/com/alibaba/excel/util/ConverterUtilsTest.java
@@ -29,10 +29,10 @@ class ConverterUtilsTest {
         // arrange
         HashMap<Integer, CellData> cellDataMap =
              new HashMap<Integer, CellData>();
-        CellData x218 = new CellData();
-        x218.setStringValue("foo");
-        x218.setType(CellDataTypeEnum.STRING);
-        cellDataMap.put(1, x218);
+        CellData x216 = new CellData();
+        x216.setStringValue("foo");
+        x216.setType(CellDataTypeEnum.STRING);
+        cellDataMap.put(1, x216);
 
         // act
         Map<Integer, String> result =
