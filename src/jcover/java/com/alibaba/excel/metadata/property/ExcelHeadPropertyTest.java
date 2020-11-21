84f9c063cb3a4518cdab0ded7d0a3f0692be3773
Diffblue CI <db-ci-platform@diffblue.com>
2020-11-21 03:37:09 +0000

    Update for version 0.6 884c329e of jcover

---
+++ src/jcover/java/com/alibaba/excel/metadata/property/ExcelHeadPropertyTest.java
@@ -0,0 +1,64 @@
+package com.alibaba.excel.metadata.property;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.Mockito.mock;
+
+import com.alibaba.excel.enums.HeadKindEnum;
+import com.alibaba.excel.metadata.Head;
+import com.alibaba.excel.metadata.Holder;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for com.alibaba.excel.metadata.property.ExcelHeadProperty
+ *
+ * @author Diffblue Cover
+ */
+
+class ExcelHeadPropertyTest {
+
+    @Test
+    void factory() {
+        Holder holder = mock(Holder.class);
+        ArrayList<List<String>> head = new ArrayList<List<String>>();
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Smith");
+        head.add(list);
+        ExcelHeadProperty excelHeadProperty =
+             new ExcelHeadProperty(holder, String.class, head, false);
+        excelHeadProperty.setContentPropertyMap(new HashMap<Integer, ExcelContentProperty>());
+        excelHeadProperty.setFieldNameContentPropertyMap(new HashMap<String, ExcelContentProperty>());
+        excelHeadProperty.setHeadClazz(String.class);
+        excelHeadProperty.setHeadKind(HeadKindEnum.NONE);
+        excelHeadProperty.setHeadMap(new HashMap<Integer, Head>());
+        excelHeadProperty.setHeadRowNumber(1);
+        excelHeadProperty.setIgnoreMap(new HashMap<String, Field>());
+        assertThat(excelHeadProperty.getContentPropertyMap().isEmpty(), is(true));
+        assertThat(excelHeadProperty.getFieldNameContentPropertyMap().isEmpty(), is(true));
+        assertThat((Class<String>) excelHeadProperty.getHeadClazz(), equalTo((Class) String.class));
+        assertThat(excelHeadProperty.getHeadKind(), is(HeadKindEnum.NONE));
+        assertThat(excelHeadProperty.getHeadMap().isEmpty(), is(true));
+        assertThat(excelHeadProperty.getHeadRowNumber(), is(1));
+        assertThat(excelHeadProperty.getIgnoreMap().isEmpty(), is(true));
+    }
+
+    @Test
+    void hasHeadReturnsFalse() {
+        Holder holder = mock(Holder.class);
+        ArrayList<List<String>> head = new ArrayList<List<String>>();
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Smith");
+        head.add(list);
+        ExcelHeadProperty excelHeadProperty =
+             new ExcelHeadProperty(holder, String.class, head, false);
+        excelHeadProperty.setHeadKind(HeadKindEnum.NONE);
+        assertThat(excelHeadProperty.hasHead(), is(false));
+    }
+}
---
+++ src/jcover/java/com/alibaba/excel/read/metadata/property/ExcelReadHeadPropertyTest.java
@@ -0,0 +1,52 @@
+package com.alibaba.excel.read.metadata.property;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.Mockito.mock;
+
+import com.alibaba.excel.enums.HeadKindEnum;
+import com.alibaba.excel.metadata.Head;
+import com.alibaba.excel.metadata.Holder;
+import com.alibaba.excel.metadata.property.ExcelContentProperty;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for com.alibaba.excel.read.metadata.property.ExcelReadHeadProperty
+ *
+ * @author Diffblue Cover
+ */
+
+class ExcelReadHeadPropertyTest {
+
+    @Test
+    void factory() {
+        Holder holder = mock(Holder.class);
+        ArrayList<List<String>> head = new ArrayList<List<String>>();
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Smith");
+        head.add(list);
+        ExcelReadHeadProperty excelReadHeadProperty =
+             new ExcelReadHeadProperty(holder, String.class, head, false);
+        excelReadHeadProperty.setContentPropertyMap(new HashMap<Integer, ExcelContentProperty>());
+        excelReadHeadProperty.setFieldNameContentPropertyMap(new HashMap<String, ExcelContentProperty>());
+        excelReadHeadProperty.setHeadClazz(String.class);
+        excelReadHeadProperty.setHeadKind(HeadKindEnum.NONE);
+        excelReadHeadProperty.setHeadMap(new HashMap<Integer, Head>());
+        excelReadHeadProperty.setHeadRowNumber(1);
+        excelReadHeadProperty.setIgnoreMap(new HashMap<String, Field>());
+        assertThat(excelReadHeadProperty.getContentPropertyMap().isEmpty(), is(true));
+        assertThat(excelReadHeadProperty.getFieldNameContentPropertyMap().isEmpty(), is(true));
+        assertThat((Class<String>) excelReadHeadProperty.getHeadClazz(), equalTo((Class) String.class));
+        assertThat(excelReadHeadProperty.getHeadKind(), is(HeadKindEnum.NONE));
+        assertThat(excelReadHeadProperty.getHeadMap().isEmpty(), is(true));
+        assertThat(excelReadHeadProperty.getHeadRowNumber(), is(1));
+        assertThat(excelReadHeadProperty.getIgnoreMap().isEmpty(), is(true));
+    }
+}
