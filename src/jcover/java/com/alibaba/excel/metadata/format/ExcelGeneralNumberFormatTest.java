cdb6051ddde2a81a878dba9675db3042b02b6603
Diffblue CI <db-ci-platform@diffblue.com>
2021-02-01 04:23:05 +0000

    Update for version 0.7 56dc3c3e of jcover

--- src/jcover/java/com/alibaba/excel/metadata/format/ExcelGeneralNumberFormatTest.java
+++ src/jcover/java/com/alibaba/excel/metadata/format/ExcelGeneralNumberFormatTest.java
@@ -1,11 +1,8 @@
 package com.alibaba.excel.metadata.format;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.anEmptyMap;
-import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.core.Is.is;
 
-import java.text.AttributedCharacterIterator;
 import java.util.Locale;
 
 import org.junit.jupiter.api.Test;
@@ -19,32 +16,9 @@ import org.junit.jupiter.api.Test;
 class ExcelGeneralNumberFormatTest {
 
     @Test
-    void formatToCharacterIterator1() {
-        AttributedCharacterIterator result =
-             new ExcelGeneralNumberFormat(new Locale("en"), false).formatToCharacterIterator(1);
-        assertThat(result.getAllAttributeKeys(), empty());
-        assertThat(result.getAttributes(), anEmptyMap());
-        assertThat(result.getRunLimit(), is(1));
-        assertThat(result.getRunStart(), is(0));
-    }
-
-    @Test
-    void formatToCharacterIterator2() {
-        AttributedCharacterIterator result =
-             new ExcelGeneralNumberFormat(new Locale("en"), true).formatToCharacterIterator(1);
-        assertThat(result.getAllAttributeKeys(), empty());
-        assertThat(result.getAttributes(), anEmptyMap());
-        assertThat(result.getRunLimit(), is(1));
-        assertThat(result.getRunStart(), is(0));
-    }
-
-    @Test
-    void formatToCharacterIterator3() {
-        AttributedCharacterIterator result =
-             new ExcelGeneralNumberFormat(new Locale("en"), false).formatToCharacterIterator(0);
-        assertThat(result.getAllAttributeKeys(), empty());
-        assertThat(result.getAttributes(), anEmptyMap());
-        assertThat(result.getRunLimit(), is(1));
-        assertThat(result.getRunStart(), is(0));
+    void format() {
+        assertThat(new ExcelGeneralNumberFormat(new Locale("en"), false).format(1), is("1"));
+        assertThat(new ExcelGeneralNumberFormat(new Locale("en"), true).format(1), is("1"));
+        assertThat(new ExcelGeneralNumberFormat(new Locale("en"), false).format(0), is("0"));
     }
 }
